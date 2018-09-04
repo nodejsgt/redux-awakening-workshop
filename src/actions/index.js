5760e96b987f1d830d24e318cf5659e281c8f1b3
wcorderolara <wcordero.lara@gmail.com>
2018-09-04 11:37:22 -0600

    add the first reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ let nextTodoId = 0;
 // ACTION TYPES
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const SET_VISIBILITI_FILTER = 'SET_VISIBILITY_FILTER';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
     SHOW_COMPLETED: 'SHOW_COMPLETED',

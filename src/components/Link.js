d0860030036d8b21799382de1e147d22b64f115c
wcorderolara <wcordero.lara@gmail.com>
2018-09-04 17:02:16 -0600

    add Presentational Components

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Link = ({ active, children, onClick }) => {
-    if (active) {
-        return <span>{children}</span>
-    }
-
-    return (
-        <a
-            href=""
-            onClick={e => {
-                e.preventDefault()
-                onClick()
-            }}
-        >
-            {children}
-        </a>
-    )
-}
+const Link = ({ active, children, onClick }) => (
+    
+    <a
+        href=""
+        onClick={e => {
+            e.preventDefault()
+            onClick()
+        }}
+    >
+        {children}
+    </a>
+)
 
 Link.propTypes = {
     active: PropTypes.bool.isRequired,
